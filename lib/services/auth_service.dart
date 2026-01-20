221726e1901118d3a8fe01212f4420b611803ad5
mplbsmakda-cmd <mplbsmakda@gmail.com>
2026-01-20 07:55:18 +0700

    Create auth_service.dart for authentication service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,14 @@
+// auth_service.dart
+
+class AuthService {
+    // Method to authenticate a user
+    Future<bool> authenticate(String username, String password) async {
+        // TODO: Implement authentication logic
+        return true;
+    }
+
+    // Method to sign out a user
+    Future<void> signOut() async {
+        // TODO: Implement sign-out logic
+    }
+}
