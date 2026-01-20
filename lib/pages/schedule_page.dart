30122c9633574876d304a6923a41397e191479f8
mplbsmakda-cmd <mplbsmakda@gmail.com>
2026-01-20 07:55:40 +0700

    Create schedule_page.dart for the schedule page

---
+++ lib/pages/schedule_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SchedulePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Schedule'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the Schedule Page',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
