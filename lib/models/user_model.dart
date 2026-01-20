f0931ba1fe3f4b055ece456084a5ea85565489f7
mplbsmakda-cmd <mplbsmakda@gmail.com>
2026-01-20 07:55:23 +0700

    Create user_model.dart for user data model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,26 @@
+// This is a user data model class
+class UserModel {
+    final String id;
+    final String name;
+    final String email;
+
+    UserModel({required this.id, required this.name, required this.email});
+
+    // Factory method to create a UserModel from a JSON map
+    factory UserModel.fromJson(Map<String, dynamic> json) {
+        return UserModel(
+            id: json['id'],
+            name: json['name'],
+            email: json['email'],
+        );
+    }
+
+    // Method to convert a UserModel to a JSON map
+    Map<String, dynamic> toJson() {
+        return {
+            'id': id,
+            'name': name,
+            'email': email,
+        };
+    }
+}
